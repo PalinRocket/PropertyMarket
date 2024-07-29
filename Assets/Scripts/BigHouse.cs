c9698fb1dd3904e71c5d98c05a549ebbb4b9b312
Paulina <Paulina@MacBook-Air-von-Tobias.fritz.box>
2024-07-29 22:30:30 +0200

    Added mouse hover over menu

--- Assets/Scripts/BigHouse.cs
+++ Assets/Scripts/BigHouse.cs
@@ -5,6 +5,9 @@ public class BigHouse : House
 {
     
     private Color blueColor = new Color(0.07355822f,0.4353032f,0.5377358f,1);
+    private bool mouseHover = false;
+    
+
     public BigHouse() : base(3,4,2)
     {
     }
@@ -13,6 +16,24 @@ public class BigHouse : House
     {
         //Set to blue color
         houseWallRenderer.material.SetColor("_Color", blueColor);
+    }
+
+    void OnMouseOver()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse is over GameObject.");
+        if(!mouseHover)
+        {
+            GetHouseProperties();
+            houseInfo.SetActive(true);
+        }
+        mouseHover = true;
+    }
+
+    void OnMouseExit()
+    {
+        mouseHover = false;
+        houseInfo.SetActive(false);
     }
 }
 
--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public abstract class House : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public abstract class House : MonoBehaviour
 
     protected GameObject houseWall;
     protected Renderer houseWallRenderer;
+    protected GameObject houseInfo;
+    protected TMP_Text houseInfoText;
     
     protected House(int bathrooms, int bedrooms, int livingrooms)
     {
@@ -21,6 +24,9 @@ public abstract class House : MonoBehaviour
     {
         houseWall = transform.GetChild(7).gameObject;
         houseWallRenderer = houseWall.GetComponent<Renderer>();
+        houseInfo = GameObject.FindGameObjectWithTag("BigHouseTextBackground");
+        houseInfoText = houseInfo.GetComponent<TMP_Text>();
+        houseInfo.SetActive(false);
     }
     
     public abstract void SetHouseColor();
@@ -29,5 +35,11 @@ public abstract class House : MonoBehaviour
     {
         //Set to white color
         houseWallRenderer.material.SetColor("_Color", new Color(1,1,1,1));
+    }
+
+    //Changes Textfield
+    protected void GetHouseProperties()
+    {
+        houseInfoText.text = $"<b>House Properties:</b>\n\nBathrooms: {bathrooms}\nBedrooms: {bedrooms}\nLivingrooms: {livingrooms}";
     }
 }
