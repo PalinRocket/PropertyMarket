3a51c1b3e0123c820b29428b201b13a4ca27f4ac
Paulina <Paulina@MacBook-Air-von-Tobias.fritz.box>
2024-08-07 13:42:00 +0200

    added script to switch back to main menu

---
+++ Assets/Scripts/BackToMenuHandler.cs
@@ -0,0 +1,30 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Collections;
+
+public class BackToMenuHandler : MonoBehaviour
+{
+    private Animator cameraAnimator;
+    private Animator fadeoutAnimator;
+    private float timeToWait = 2f;
+
+    public void Awake()
+    {
+        fadeoutAnimator = GameObject.FindWithTag("Canvas").GetComponent<Animator>();
+        cameraAnimator = GameObject.FindWithTag("MainCamera").GetComponent<Animator>();
+    } 
+
+    public void LoadGameScene()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex - 1));
+    }
+
+    IEnumerator LoadLevel(int index)
+    {
+        fadeoutAnimator.SetTrigger("CanvasFadeout");
+        cameraAnimator.SetTrigger("MoveCameraBack");
+        
+        yield return new WaitForSeconds(timeToWait);
+        SceneManager.LoadScene(index);
+    }
+}
